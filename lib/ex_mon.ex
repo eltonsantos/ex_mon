cdaf661c61309e62c3a335bd7b54eafe602d2236
Elton Santos <eltonaxl@hotmail.com>
2023-08-05 16:35:51 -0300

    Print messages

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -1,7 +1,18 @@
 defmodule ExMon do
-  alias ExMon.Player
+  alias ExMon.Game.Status
+  alias ExMon.{Game, Player}
+
+  @computer_name "Robotinik"
 
   def create_player(name, move_rnd, move_avg, move_heal) do
     Player.build(name, move_rnd, move_avg, move_heal)
+  end
+
+  def start_game(player) do
+    @computer_name
+    |> create_player(:punch, :kick, :heal)
+    |> Game.start(player)
+
+    Status.print_round_message()
   end
 end
