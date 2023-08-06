80c46a9cfb8df36f0de0610eec79bcebcd275908
Elton Santos <eltonaxl@hotmail.com>
2023-08-06 02:03:27 -0300

    Print messages

--- lib/game/actions/attack.ex
+++ lib/game/actions/attack.ex
@@ -1,4 +1,5 @@
 defmodule ExMon.Game.Actions.Attack do
+  alias ExMon.Game.Status
   alias ExMon.Game
 
   @move_avg_power 18..25
@@ -12,7 +13,7 @@ defmodule ExMon.Game.Actions.Attack do
     |> Game.fetch_player()
     |> Map.get(:life)
     |> calculate_total_life(damage)
-    |> update_opponent_life(opponent)
+    |> update_opponent_life(opponent, damage)
   end
 
   defp calculate_power(:move_avg), do: Enum.random(@move_avg_power)
@@ -21,11 +22,13 @@ defmodule ExMon.Game.Actions.Attack do
   defp calculate_total_life(life, damage) when life - damage < 0, do: 0
   defp calculate_total_life(life, damage), do: life - damage
 
-  defp update_opponent_life(life, opponent) do
-    opponent |> Game.fetch_player() |> Map.put(:life, life) |> update_game(opponent)
+  defp update_opponent_life(life, opponent, damage) do
+    opponent |> Game.fetch_player() |> Map.put(:life, life) |> update_game(opponent, damage)
   end
 
-  defp update_game(player, opponent) do
+  defp update_game(player, opponent, damage) do
     Game.info() |> Map.put(opponent, player) |> Game.update()
+
+    Status.print_move_message(opponent, :attack, damage)
   end
 end
