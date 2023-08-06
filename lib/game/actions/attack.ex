4b7c30ef6883f05f86934fec2f79556d23215d15
Elton Santos <eltonaxl@hotmail.com>
2023-08-06 16:30:56 -0300

    Print messages

--- lib/game/actions/attack.ex
+++ lib/game/actions/attack.ex
@@ -5,7 +5,6 @@ defmodule ExMon.Game.Actions.Attack do
   @move_avg_power 18..25
   @move_rnd_power 10..35
 
-  @spec attack_opponent(any, :move_avg | :move_rnd) :: any
   def attack_opponent(opponent, move) do
     damage = calculate_power(move)
 
